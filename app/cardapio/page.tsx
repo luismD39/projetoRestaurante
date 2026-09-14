eeb0ad87a4521d69c3a39806ff07235a133db6e3
ALUNO SESI PETROLINA <luis.dsousa39@gmail.com>
2026-09-14 14:21:40 -0300

    atualizado

--- app/cardapio/page.tsx
+++ app/cardapio/page.tsx
@@ -39,7 +39,7 @@ export default function CardapioPage(){
     }, [])
 
     return(
-        <main className="p-8 bg-amber-50 max-h-max">
+        <main className="p-8 bg-amber-50 min-h-screen">
             <h1 className="mb-6 text-3xl font-bold text-black">Cardapio</h1>
 
             {loading ? (<p>Carregando produtos...</p>) : (
